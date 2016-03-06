6da5a215818871c140540d37478471151a884bd9
DongHyun Kim <knight2995@naver.com>
2016-03-06 20:33:06 +0900

    0306

--- myJS.js
+++ myJS.js
@@ -1,5 +1,17 @@
 function loadData3()
 {
 
-	return 0;
+	$("#contents-3").load("external/3.html");
+	$("#button3").remove();
+}
+function loadData4()
+{
+	$("#contents-4").load("external/4.html");
+
+
+}
+function loadData4Translate()
+{
+	$("#contents-4").load("external/4-translate.html");
+
 }
